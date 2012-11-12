cc0daaa127d7fe030134584e0b9e5a176b0e2480
Hernan Ciudad <hernan@myemma.com>
2012-11-12 16:11:58 -0600

    Small fix to setup.py to install inlinestyle package
    Previously, all the dependencies would be installed, but not inlinestyle itself.
    
    Also, added two items to .gitignore and bumped the version to 0.3.

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="inlinestyle",
-      version="0.2",
+      version="0.3",
       description="convert html style blocks to inline style statements",
       author="Matt George",
+      packages=find_packages('src', exclude=['tests']),
+      package_dir={'': 'src'},
       author_email="mgeorge@gmail.com",
       install_requires=["beautifulsoup4", "nose", "cssutils"])
